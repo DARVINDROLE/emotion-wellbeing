0738b34b7d2c9cc60ecbcab308c67b61012d2506
DARVINDROLE <127033248+DARVINDROLE@users.noreply.github.com>
2025-06-25 11:21:52 +0530

    Delete services directory

--- services/firebase.py
+++
@@ -1,9 +0,0 @@
-# services/firebase.py
-import firebase_admin
-from firebase_admin import credentials, firestore
-
-# Initialize Firebase app only once
-cred = credentials.Certificate("firebase_service_account.json")  # path to your downloaded key
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
