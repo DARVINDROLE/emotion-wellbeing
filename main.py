d09613e8931a39915d0ea202984eb8300615a113
DARVINDROLE <127033248+DARVINDROLE@users.noreply.github.com>
2025-06-26 00:13:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 # CORS Middleware — ✅ Must come before routers
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
